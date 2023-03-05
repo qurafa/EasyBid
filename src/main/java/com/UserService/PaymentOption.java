37ca421f390068efc649890b22e140c78bc30e7b
Hashir Jamil <hashir.jamil17@gmail.com>
2023-03-04 21:22:00 -0500

    Houston we have a service...

---
+++ src/main/java/com/UserService/PaymentOption.java
@@ -0,0 +1,8 @@
+package com.UserService;
+
+public abstract class PaymentOption {
+    private String cardNumber;
+    private String cardHolderName;
+    private String expiryDate;
+    private String CVC;
+}
