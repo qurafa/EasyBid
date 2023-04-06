ce8b48b4936baf2e01b3c43b2cbbb9d8ee617c08
Faruq Afolabi <gbolayo13@gmail.com>
2023-04-05 23:05:20 -0400

    push

--- EasyBid/UserService/src/main/java/UserService/PaymentOption.java
+++
@@ -1,8 +0,0 @@
-package UserService;
-
-public abstract class PaymentOption {
-    private String cardNumber;
-    private String cardHolderName;
-    private String expiryDate;
-    private String CVC;
-}
